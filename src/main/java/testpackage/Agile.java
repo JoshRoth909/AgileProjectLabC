4790a611289f9598e45916083329825e5ddf436d
User <user@Users-Macintosh.local>
2020-10-26 18:08:10 -0400

    added some test

---
+++ src/main/java/testpackage/Agile.java
@@ -0,0 +1,12 @@
+package testpackage;
+
+public class Agile {
+  public static int[] invert(int[] test) {
+      for (int i = 0; i< test.length;i++){
+          test[i] = test[i] * -1;
+
+      }
+     return test;
+  }
+}
+
--- src/test/java/AgileTest.java
+++ src/test/java/AgileTest.java
@@ -1,13 +1,45 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+import static testpackage.Agile.invert;
 
 public class AgileTest {
 
-    @Test
-    void test(){
-        fail("This Failed");
-    }
 
+
+        @Test
+        @DisplayName("When given an empty array we expect back an empty array.")
+        void testEmptyArray(){
+            int[] expected = {};
+            int[] emptyArray = {};
+            int[] actual = invert(emptyArray);
+            assertArrayEquals(expected,actual);
+
+        }
+        @Test
+        @DisplayName("when given an array of zeros we expect an array of zeros to be returned.")
+        void testZeroArray(){
+            int[] expected= {0};
+            int[] zeroArray={0};
+            int[] actual= invert(zeroArray);
+            assertArrayEquals(expected,actual);
+
+        }
+        @Test
+        @DisplayName("when given an array of one positive number we expect back a negative number.")
+        void testInverseOfOnePositiveNumber(){
+            int[] expected = {-1};
+            int[] positive = {1};
+            int[] actual = invert(positive);
+            assertArrayEquals(expected,actual);
+        }
+//    when given an array of one negative number we expect back a positive number.
+//    when given an array of multiple positive numbers we expect back multiple negative numbers.
+//    when given an array of multiple negative numbers we expect back multiple positive numbers.
+//    when given an array of multiple mixed numbers we expect back multiple inverse mixed numbers.
 
 }
