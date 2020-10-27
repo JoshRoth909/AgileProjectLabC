47bfe898ce5aea4f0ffe1442c5a95ec9bbc4ab4f
User <user@Users-Macintosh.local>
2020-10-27 16:13:55 -0400

    removed whitespace and added indentions

--- src/main/java/testpackage/Agile.java
+++ src/main/java/testpackage/Agile.java
@@ -1,12 +1,9 @@
 package testpackage;
-
 public class Agile {
   public static int[] invert(int[] test) {
       for (int i = 0; i< test.length;i++){
           test[i] = test[i] * -1;
-
       }
      return test;
   }
 }
-
--- src/test/java/AgileTest.java
+++ src/test/java/AgileTest.java
@@ -1,16 +1,8 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 import static testpackage.Agile.invert;
-
 public class AgileTest {
-
-
-
         @Test
         @DisplayName("When given an empty array we expect back an empty array.")
         void testEmptyArray(){
@@ -18,7 +10,6 @@ public class AgileTest {
             int[] emptyArray = {};
             int[] actual = invert(emptyArray);
             assertArrayEquals(expected,actual);
-
         }
         @Test
         @DisplayName("when given an array of zeros we expect an array of zeros to be returned.")
@@ -27,7 +18,6 @@ public class AgileTest {
             int[] zeroArray={0};
             int[] actual= invert(zeroArray);
             assertArrayEquals(expected,actual);
-
         }
         @Test
         @DisplayName("when given an array of one positive number we expect back a negative number.")
@@ -37,43 +27,39 @@ public class AgileTest {
             int[] actual = invert(positive);
             assertArrayEquals(expected,actual);
         }
-
-@Test
-@DisplayName("when given an array of one positive number we expect back a negative number.")
-void testInverseOfOneNegativeNumber(){
-    int[] expected = {1};
-    int[] negative = {-1};
-    int[] actual = invert(negative);
-    assertArrayEquals(expected,actual);
-}
-
-@Test
-@DisplayName("when given an array of multiple positive " +
-        "numbers we expect back multiple negative numbers.")
-void testInverseOfMultiplePositiveNumbers(){
-    int[] expected = {-1,-1,-1,-1};
-    int[] positive = {1,1,1,1};
-    int[] actual = invert(positive);
-    assertArrayEquals(expected,actual);
-}
-
-@Test
-@DisplayName("when given an array of multiple positive " +
-        "numbers we expect back multiple negative numbers.")
-void testInverseOfMultipleNegativeNumbers(){
-    int[] expected = {1,1,1,1};
-    int[] negative = {-1,-1,-1,-1};
-    int[] actual = invert(negative);
-    assertArrayEquals(expected,actual);
-}
-
-@Test
-@DisplayName("when given an array of multiple positive and negative " +
-        "numbers we expect back multiple positive and negative numbers.")
-void testInverseOfMultipleMixedNumbers(){
-    int[] expected = {1,-1,1,-1};
-    int[] mixed = {-1,1,-1,1};
-    int[] actual = invert(mixed);
-    assertArrayEquals(expected,actual);
-}
-}
+        @Test
+        @DisplayName("when given an array of one positive number we expect back a negative number.")
+        void testInverseOfOneNegativeNumber(){
+            int[] expected = {1};
+            int[] negative = {-1};
+            int[] actual = invert(negative);
+            assertArrayEquals(expected,actual);
+        }
+        @Test
+        @DisplayName("when given an array of multiple positive " +
+                "numbers we expect back multiple negative numbers.")
+        void testInverseOfMultiplePositiveNumbers(){
+            int[] expected = {-1,-1,-1,-1};
+            int[] positive = {1,1,1,1};
+            int[] actual = invert(positive);
+            assertArrayEquals(expected,actual);
+        }
+        @Test
+        @DisplayName("when given an array of multiple positive " +
+                "numbers we expect back multiple negative numbers.")
+        void testInverseOfMultipleNegativeNumbers(){
+            int[] expected = {1,1,1,1};
+            int[] negative = {-1,-1,-1,-1};
+            int[] actual = invert(negative);
+            assertArrayEquals(expected,actual);
+        }
+        @Test
+        @DisplayName("when given an array of multiple positive and negative " +
+                "numbers we expect back multiple positive and negative numbers.")
+        void testInverseOfMultipleMixedNumbers(){
+            int[] expected = {1,-1,1,-1};
+            int[] mixed = {-1,1,-1,1};
+            int[] actual = invert(mixed);
+            assertArrayEquals(expected,actual);
+        }
+        }
